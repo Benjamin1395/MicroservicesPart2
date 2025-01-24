92376500d818f208a3cdfd19718a4d8059978590
Benjamin1395 <52389293+Benjamin1395@users.noreply.github.com>
2025-01-24 08:02:17 -0500

    Delete First Microservice directory

--- First Microservice/microservices-pucp-users/src/main/java/com/concept/pucp/microservices/app/users/models/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.concept.pucp.microservices.app.users.models.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.concept.pucp.microservices.app.users.models.entity.User;
-
-public interface UserRepository extends CrudRepository <User,Long> {
-
-}
